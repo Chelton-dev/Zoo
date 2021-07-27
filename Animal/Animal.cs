4ce4fb38d0248c128118b6f8ffe366c08a8ab483
Anh Nguyen <anguyen@swin.edu.au>
2021-07-27 11:28:57 +1000

    Write up unit tests for Animal and Staff

---
+++ Animal/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Animal
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public string Sound { get; set; }
+
+        public string MakeSound() {
+            return $"{this.Name} makes a {this.Sound} sound";
+        }
+    }
+}
---
+++ Staff/Staff.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Staff
+{
+    public class Staff
+    {
+        public string Name { get; set; }
+        public string Job { get; set; }
+        public int YearOfBirth { get; set; }
+
+        public string GetDetails() {
+            return $"{this.Name} is a {this.Name}";
+        }
+
+        public int GetAge() {
+            return 2021 - this.YearOfBirth;
+        }
+    }
+}
